9a3ef7899a470329fbb158b24ee7a2ff332fdb4c
ismailyurek <ismail.yurek@gmail.com>
2014-10-01 16:26:41 +0300

    new modification files added

---
+++ upload/system/modification/system/startup.php
@@ -0,0 +1,13 @@
+<?php 
+
+require_once(DIR_SYSTEM . 'startup.php');
+
+function theme($file){
+	return '/catalog/view/theme/' . $file;
+	global $tenant_id;
+	return 'repository/tenant_' . $tenant_id . '/theme/' . $file;
+}
+function js($file){
+	global $tenant_id;
+	return 'repository/tenant_' . $tenant_id . '/theme/' . $file;
+} 
